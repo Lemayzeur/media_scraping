ddceae589fcadd3d496089e06c671ada3fa3cd42
Lub Lorry Lamysère <lemayzeur@code9haiti.com>
2022-03-15 21:31:44 -0400

    Scraping script to copy

---
+++ ie-scraping.py
@@ -0,0 +1,56 @@
+import requests
+import random
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+
+free_proxy_url = '<http://proxy_list>' # TODO
+
+# get the proxy as http response
+# TODO
+
+# Convert the http response to list of proxy
+# TODO
+
+def get_random_proxy(array):
+	''' get a random proxy '''
+	proxy = random.choice(array)
+
+	# to get more info on the proxy format
+	# More info, visit: https://docs.python-requests.org/en/latest/api/#module-requests
+	return # {'protocol': 'ip:port'} # TODO
+
+def get_working_proxy():
+	''' test random proxies, to get one that works'''
+	TOTAL_TESTS = 20
+	for _ in range(TOTAL_TESTS):
+		proxy = get_random_proxy(<proxy_list>) # TODO
+		try:
+			requests.get('<test_url>', proxies=proxy) # TODO
+			# if it works, return it
+			return proxy
+		except Exception as error:
+			print(error)
+
+
+# set a custom header
+headers = {
+	'User-Agent': '<browser>' # TODO
+}
+
+# call and get the proxy
+proxy = get_working_proxy()
+if proxy:
+
+	# hit the target site
+	res = requests.get('<target_site>', headers=headers, proxies=proxy) # TODO
+
+	# init the beautiful instance as html parser
+	soup = bs(<target_content> , '<parser>') # TODO
+
+	# After collecting the data needed, convert it to pandas dataframe
+  # TODO
+
+	# then export it as csv
+	df.to_csv('<path/file_name>') # TODO
+else:
+	print('No working proxy found. Go buy some instead')
