ba18aab49672ff9d24ce66f48c7f16c0be47b5e3
RebecaCazeau <rebecacazeau7@gmail.com>
2022-03-22 14:39:56 -0400

    2eme commit: recuperation du contenu des sites cibles/Test

--- ie-scraping.py
+++ ie-scraping.py
@@ -18,9 +18,9 @@ response_proxy = requests.get(free_proxy_url)
 proxy_list = response_proxy.text.strip().split("\n")
 
 
-print(proxy_list)
+#print(proxy_list)
 
-"""
+
 
 def get_random_proxy(array):
 	''' get a random proxy '''
@@ -28,36 +28,64 @@ def get_random_proxy(array):
 
 	# to get more info on the proxy format
 	# More info, visit: https://docs.python-requests.org/en/latest/api/#module-requests
-	return # {'protocol': 'ip:port'} # TODO
+
+	return{
+	'http':proxy,
+
+
+	} # {'protocol': 'ip:port'} # TODO
 
 def get_working_proxy():
 	''' test random proxies, to get one that works'''
 	TOTAL_TESTS = 20
-	for _ in range(TOTAL_TESTS):
-		proxy = get_random_proxy(<proxy_list>) # TODO
+	for i in range(TOTAL_TESTS):
+		proxy = get_random_proxy(proxy_list) # TODO
 		try:
-			requests.get('<test_url>', proxies=proxy) # TODO
+			requests.get('https://www.google.com/', proxies=proxy) # TODO
 			# if it works, return it
+
 			return proxy
 		except Exception as error:
 			print(error)
 
 
+
 # set a custom header
+
 headers = {
-	'User-Agent': '<browser>' # TODO
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:96.0) Gecko/20100101 Firefox/96.0' # TODO
 }
 
 # call and get the proxy
 proxy = get_working_proxy()
+
+#Test
+#print(proxy)
+
+
+url_list = ["https://lenouvelliste.com/","https://www.haitilibre.com/"]
+
+keyWords = ["kidnapping","Kidnapping","kidnape","Kidnape","kidnaper","Kidnaper","Viol","viol","agression sexuelle","Agression sexuelle","Agressions sexuelles","agressions sexuelles",
+"Fusillade","fusillade","Echange de tirs","echange de tirs","Coup de feu","coup de feu","Assassinat","assassinat","crime","Crime","meurtre","meurtres","Meurtre","Meurtres",
+"Jovenel","Jovenel Moise","Martine","Martine Moise"]
+
 if proxy:
 
-	# hit the target site
-	res = requests.get('<target_site>', headers=headers, proxies=proxy) # TODO
+	for site in url_list:
+		res = requests.get(site, headers=headers, proxies=proxy) # TODO
 
-	# init the beautiful instance as html parser
-	soup = bs(<target_content> , '<parser>') # TODO
+		resContents = res.content
 
+		#htmlContent = res.content
+
+		print(resContents)
+
+		# init the beautiful instance as html parser
+
+		#soup = bs(resContents,'html.parser') # TODO
+	
+	
+"""
 	# After collecting the data needed, convert it to pandas dataframe
   # TODO
 
@@ -67,3 +95,5 @@ else:
 	print('No working proxy found. Go buy some instead')
 
 """
+
+
