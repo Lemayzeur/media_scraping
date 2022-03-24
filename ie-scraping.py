ddedb9d790ffb4764f99672f2c5649cf6595a285
Jeudy <jeudydavid2@gmail.com>
2022-03-24 05:53:15 -0400

    my first commit

--- ie-scraping.py
+++ ie-scraping.py
@@ -1,15 +1,19 @@
+from attr import attr
 import requests
 import random
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 
-free_proxy_url = '<http://proxy_list>' # TODO
+free_proxy_url = 'https://github.com/clarketm/proxy-list/blob/master/proxy-list-raw.txt' # TODO
 
 # get the proxy as http response
 # TODO
-
+r = requests.get(free_proxy_url)
 # Convert the http response to list of proxy
 # TODO
+soup = bs(r.text, "html.parser").find_all("td", {"class": "blob-code blob-code-inner js-file-line"})
+
+proxy = [proxy.text for proxy in soup]
 
 def get_random_proxy(array):
 	''' get a random proxy '''
@@ -17,40 +21,64 @@ def get_random_proxy(array):
 
 	# to get more info on the proxy format
 	# More info, visit: https://docs.python-requests.org/en/latest/api/#module-requests
-	return # {'protocol': 'ip:port'} # TODO
+	return {'http': proxy}
 
 def get_working_proxy():
 	''' test random proxies, to get one that works'''
 	TOTAL_TESTS = 20
 	for _ in range(TOTAL_TESTS):
-		proxy = get_random_proxy(<proxy_list>) # TODO
+		prox = get_random_proxy(proxy) # TODO
 		try:
-			requests.get('<test_url>', proxies=proxy) # TODO
+			requests.get('https://www.google.com/', proxies=prox) # TODO
 			# if it works, return it
-			return proxy
+			return prox
 		except Exception as error:
 			print(error)
-
+# call and get the proxy
+# p = get_working_proxy()
+# print(p)
 
 # set a custom header
 headers = {
-	'User-Agent': '<browser>' # TODO
+	'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Mobile/15E148 Safari/604.1' # TODO
 }
 
-# call and get the proxy
-proxy = get_working_proxy()
-if proxy:
-
-	# hit the target site
-	res = requests.get('<target_site>', headers=headers, proxies=proxy) # TODO
-
-	# init the beautiful instance as html parser
-	soup = bs(<target_content> , '<parser>') # TODO
-
+nombres_pages = 1
+# res = requests.get('https://lenouvelliste.com/national?page=1', headers=headers, proxies=p) # TODO
+# print(res.text)
+for page in range(0,nombres_pages):
+	if proxy:
+		prox = get_working_proxy()
+		res = requests.get('https://lenouvelliste.com/national?page='+str(page+1), headers=headers, proxies=prox) # TODO
+		# print(res.text)
+        
+		soup = bs(res.text, 'html.parser') 
+        
+        
+		lien = soup.find_all('a', attrs={'class':'mrf-buttonOpacityLight mrf-button mrf-buttonArticle mrf-hidden-link'})
+		lien = [link.get('href') for link in lien]
+		filename="NEWS.csv"
+		f=open(filename,"w", encoding = 'utf-8')
+		entete="Article, url, date, KNPG_M, TM_KNPG, V_M, TM_V, FSD_M, TM_FSD, ASST_M, TM_ASST, JVL_M TM_JVL\n"
+		f.write(entete)
+ 
+		for i in lien:
+			res = requests.get(i)
+			soup = bs(res.text, 'html.parser') 
+			article = soup.find('div', {'class':'detail_content_area'}).find('h2').text
+			url = soup.find('div', {'class':'auteur_right pull-right'}).find('a')['href']
+			print(article)
+			
+			
+					
+		# init the beautiful instance as html parser
+		# soup = bs(res , 'html.parser') # TODO
+  
+        
 	# After collecting the data needed, convert it to pandas dataframe
   # TODO
 
 	# then export it as csv
-	df.to_csv('<path/file_name>') # TODO
-else:
-	print('No working proxy found. Go buy some instead')
+	# df.to_csv('<path/file_name>') # TODO
+# else:
+# 	print('No working proxy found. Go buy some instead')
