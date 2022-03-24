c9573885d0a85b2952cf509ec9ebed4e529c942a
carlr <carlritchyalexis99@gmail.com>
2022-03-24 18:21:37 -0400

    WebScraping3

--- ie-scraping.py
+++ ie-scraping.py
@@ -1,10 +1,9 @@
 import requests
 import random
-import pandas as pd
+import time
 from bs4 import BeautifulSoup as bs
 
-# TODO
-proxyUrl = 'https://github.com/clarketm/proxy-list/blob/master/proxy-list-raw.txt'
+proxyUrl = 'https://github.com/clarketm/proxy-list/blob/master/proxy-list-raw.txt'  # TODO
 
 # get the proxy as http response
 # TODO
@@ -20,7 +19,7 @@ for ip in urlToText.find_all('td', class_='blob-code blob-code-inner js-file-lin
 
 
 def get_random_proxy(array):
-    # get a random proxy
+    ''' get a random proxy '''
     proxy = random.choice(array)
     # to get more info on the proxy format
     # More info, visit: https://docs.python-requests.org/en/latest/api/#module-requests
@@ -28,7 +27,7 @@ def get_random_proxy(array):
 
 
 def get_working_proxy():
-    # test random proxies, to get one that works'''
+    ''' test random proxies, to get one that works'''
     TOTAL_TESTS = 20
     for _ in range(TOTAL_TESTS):
         proxy = get_random_proxy(listOfProxy)  # TODO
@@ -51,25 +50,49 @@ headers = {
 # call and get the proxy
 proxy = get_working_proxy()
 
-targetSite = [
-    'https://lenouvelliste.com/national?page=1',
+targetList = [
     'https://lenouvelliste.com/national?page=2',
     'https://www.haitilibre.com/flash-infos-1.html',
     'https://www.haitilibre.com/flash-infos-2.html'
 ]
 
+keywordList = [
+    'kidnapping', 'viol', 'agression sexuelle', 'fusillade', 'enlevement', 'insécurité',
+    'séquestration', 'échange de tir', 'coup de feu', 'affrontement', 'assassinat', 'hommes armés'
+                                                                                    'homicide', 'crime', 'meurtre',
+    'Jovenel', 'Jovenel Moïse', 'Martine Moïse',
+    'suicide'
+]
+
+pages = 10
+dataset = []
 if proxy:
+    # hit the target site
+    for page in range(0, pages):
+        pageRequest = requests.get(f'https://lenouvelliste.com/national?page={page + 1}', headers=headers,
+                                   proxies=proxy)  # TODO
+        # init the beautiful instance as html parser
+        pageContent = bs(pageRequest.text, 'html.parser')  # TODO
+        # fnd the class that contain the link i searching
+        for content in pageContent.find_all('div', class_='content_widget'):
+            # finding the link
+            for titleText in content.find_all('a', href=True):
+                for link in range(0, len(keywordList)):
+                    if keywordList[link] in titleText["href"]:
+                        keywordLink = requests.get(titleText["href"])
+                        keywordResponse = bs(keywordLink, 'html.parser')
+                        print(f' href content: {titleText["href"]}')
 
-# hit the target site
-# res = requests.get('<target_site>', headers=headers, proxies=proxy) # TODO
+            articleTitle = titleText.contents[0]
+            for keyword in range(0, len(keywordList)):
+                if keywordList[keyword] in articleTitle:
+                    print(f'{articleTitle}')
+        time.sleep(5)
+    # After collecting the data needed, convert it to pandas dataframe
+    # TODO
 
-# init the beautiful instance as html parser
-# soup = bs('<target_content>' , '<parser>') # TODO
+    # then export it as csv
+    # df.to_csv('<path/file_name>') # TODO
 
-# After collecting the data needed, convert it to pandas dataframe
-# TODO
-
-# then export it as csv
-# 	df.to_csv('<path/file_name>') # TODO
 # else:
-# 	print('No working proxy found. Go buy some instead')
+# 	print('No working proxy found. Go buy some instead'
